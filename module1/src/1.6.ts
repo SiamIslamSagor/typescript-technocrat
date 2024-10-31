cb1b9aa60b784fa5907efcb379967a796d3389e9
SiamIslamSagor <msiamislam12345@gmail.com>
2024-11-01 05:05:18 +0600

    explore function types and method in tsc

---
+++ module1/src/1.6.ts
@@ -0,0 +1,24 @@
+// Learning function
+// Normal Function
+// Arrow Function
+
+function add(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+add(2, 2);
+
+const arrowAdd = (num1: number, num2: number): number => num1 + num2;
+
+// Object > function --> it's called method
+const poorUser = {
+  name: "Siam",
+  balance: 0,
+  addBalance(balance: number): string {
+    return `My new balance is: ${this.balance + balance}`;
+  },
+};
+
+const arr: number[] = [2, 4, 2, 5, 6, 3];
+
+const newArr: number[] = arr.map((el: number): number => el * el);
